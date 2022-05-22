027cba2dc54d54716c054124e56f5787f40917cd
Raushan Kumar <raushansft@gmail.com>
2022-05-22 13:03:11 +0530

    all functionality done except update

--- index.js
+++ index.js
@@ -21,8 +21,10 @@ async function updateDom(){
     th6.textContent="COHORT";
     let th7=document.createElement("th");
     th7.textContent="DELETE";
+    let th8=document.createElement("th");
+    th8.textContent="Update";
 
-    theadRow.append(th1,th2,th3,th4,th5,th6,th7);
+    theadRow.append(th1,th2,th3,th4,th5,th6,th7,th8);
     thead.append(theadRow);
 
 
@@ -56,15 +58,46 @@ async function updateDom(){
         let deleteButton=document.createElement("button");
         deleteButton.textContent="Delete";
         deleteButton.onclick= async function(){
+           try {
             let res=await fetch(`http://localhost:3000/students/${id}`,{
                 method:"delete"
             });
             // updateDom();
+               
+           } catch (error) {
+               console.log(error, "Delete Error");
+           }
+
+           alert("Delete Sucessfull")
         }
         td7.append(deleteButton);
 
         
-        row.append(td1,td2,td3,td4,td5,td6,td7);
+        let td8=document.createElement("td");
+        let updateButton=document.createElement("button");
+        updateButton.textContent="Update";
+        updateButton.onclick= async function(){
+           try {
+               let data= await fetch(`http://localhost:3000/students/${id}`);
+               let tempdata=data.json();
+            //    console.log(tempdata);
+            localStorage.setItem("tempData",JSON.stringify(tempdata));
+
+            // let res=await fetch(`http://localhost:3000/students/${id}`,{
+            //     method:"PATCH"
+            // });
+            // // updateDom();
+               
+           } catch (error) {
+               console.log(error, "UPDATE Error");
+           }
+
+           alert("Update Sucessfull")
+        }
+        td8.append(updateButton);
+
+        
+        row.append(td1,td2,td3,td4,td5,td6,td7,td8);
         tbody.append(row);
 
         
@@ -75,5 +108,40 @@ async function updateDom(){
     container.append(table);
 }
 
+// call update dom function
 updateDom();
-console.log("hello")
+
+
+
+
+// update details function
+async function updateDetails(){
+    try {
+        let name=document.querySelector("#name").value;
+    let age=+document.querySelector("#age").value;
+    let gender=document.querySelector("#gender").value;
+    let mark=+document.querySelector("#mark").value;
+    let cohort=document.querySelector("#cohort").value;
+
+    let body={
+        name,age,gender,mark,cohort
+    }
+
+    let res=await fetch(` http://localhost:3000/students`,{
+    method:"POST",
+    body: JSON.stringify(body),
+    headers: {
+        "Content-Type" : "application/json"
+    }
+    });
+    alert("New Details Added")
+
+    // let data=await res.json();
+    // console.log(data);
+        
+    } catch (error) {
+        console.log(error);
+    }
+
+    
+}
